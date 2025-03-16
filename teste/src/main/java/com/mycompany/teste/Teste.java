964ebdd40caa76d6a8764a1b4463a681596230fd
jo-o-victor <jo-o-victor@jo-o-victor-VJFE41F11X-B0411H>
2025-03-16 17:19:38 -0300

    Commit inicial. Foram criados o arquivo pom.xml, teste.java e aaaaaa.java

---
+++ teste/src/main/java/com/mycompany/teste/Teste.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste;
+
+/**
+ *
+ * @author jo-o-victor
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        aaaaaa teste = new aaaaaa();
+        teste.teste();
+    }
+}
---
+++ teste/src/main/java/com/mycompany/teste/aaaaaa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.teste;
+
+/**
+ *
+ * @author jo-o-victor
+ */
+public class aaaaaa {
+    public void teste(){
+    System.out.println("Hello World!");
+}
+}
